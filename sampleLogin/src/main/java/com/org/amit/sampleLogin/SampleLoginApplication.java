ec7ff4f656faf25a05791625659a6effac17733f
Amit Ghosh <79380006+amitghosh1990@users.noreply.github.com>
2021-02-22 01:56:50 +0530

    Swagger configuration

--- sampleLogin/src/main/java/com/org/amit/sampleLogin/SampleLoginApplication.java
+++ sampleLogin/src/main/java/com/org/amit/sampleLogin/SampleLoginApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author AMIT
@@ -12,7 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 
 @SpringBootApplication
-@EnableSwagger2
 public class SampleLoginApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
